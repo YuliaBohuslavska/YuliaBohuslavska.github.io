3e201f3470bc1ec0e2a296c9dbe53a69358c29a6
YuliaBohuslavska <77485540+YuliaBohuslavska@users.noreply.github.com>
2021-01-26 13:14:17 +0200

    26.01 upload new files

---
+++ MyFlashcards/script.js
@@ -0,0 +1,19 @@
+/*
+
+let card = document.querySelectorAll('.card');
+let cardFront = document.querySelectorAll('.card-front');
+let cardBack = document.querySelectorAll('.card-back');
+for (let i = 0; i < card.length; i++) {
+    cardFront[i].onclick = function () {
+        card[i].style.transform = 'rotateY(180deg)';
+        cardFront[i].style.visibility = 'hidden';
+        cardBack[i].style.visibility = 'visible';
+    }
+    cardBack[i].onclick = function () {
+        card[i].style.transform = 'rotateY(180deg)';
+        cardBack[i].style.visibility = 'hidden';
+        cardFront[i].style.visibility = 'visible';
+    }
+}
+
+*/
